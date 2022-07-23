ed168aac610f8d81118eacaa25b7204a3b211495
Federico Cecchetto <fedecek3@gmail.com>
2022-07-24 01:37:01 +0200

    Add seektable if missing + other changes

--- src/tool.cpp
+++ src/tool.cpp
@@ -4,6 +4,7 @@
 
 #include <utils/io.hpp>
 #include <utils/flags.hpp>
+#include <utils/string.hpp>
 
 #define APPLICATION_ID "fsiz"
 #define CONSTANT_BLOCKSIZE 0x400
@@ -35,6 +36,7 @@ namespace tool
 		{
 			metadata_block_header_t header;
 			char* data;
+			char* start;
 		};
 
 		bool check_signature(const std::string& buffer)
@@ -57,16 +59,6 @@ namespace tool
 			return true;
 		}
 
-		unsigned int little_endian(unsigned int be)
-		{
-			return  (be << 24) | ((be << 8) & 0x00FF0000) | ((be >> 8) & 0x0000FF00) | (be >> 24);
-		}
-
-		uint16_t little_endian(uint16_t be)
-		{
-			return ntohs(be);
-		}
-
 		metadata_block_t parse_metadata_block(char* buffer)
 		{
 			/*
@@ -78,7 +70,7 @@ namespace tool
 				  24   block length (header not included)
 			*/
 
-			const auto header = little_endian(*reinterpret_cast<uint32_t*>(
+			const auto header = _byteswap_ulong(*reinterpret_cast<uint32_t*>(
 				reinterpret_cast<uint64_t>(buffer)));
 
 			const auto is_last = static_cast<bool>(header >> (8 * 3 + 7));
@@ -90,6 +82,11 @@ namespace tool
 			block.header.type = block_type;
 			block.header.length = block_length;
 			block.data = buffer + 4;
+			block.start = buffer;
+
+#ifdef DEBUG
+			printf("Found block of type %i\n", block.header.type);
+#endif
 
 			return block;
 		}
@@ -99,38 +96,35 @@ namespace tool
 			*block = parse_metadata_block(buffer);
 		}
 
-		bool verify_streaminfo_block(const metadata_block_t& block)
+		void verify_streaminfo_block(const metadata_block_t& block)
 		{
 			if (utils::flags::has_flag("-ignore-blocksize") || utils::flags::has_flag("i"))
 			{
-				return true;
+				return;
 			}
 
-			const auto minimum = little_endian(*reinterpret_cast<uint16_t*>(block.data));
-			const auto maximum = little_endian(*reinterpret_cast<uint16_t*>(block.data + 2));
-			const auto res = (maximum == CONSTANT_BLOCKSIZE && minimum == CONSTANT_BLOCKSIZE);
+			const auto minimum = _byteswap_ushort(*reinterpret_cast<uint16_t*>(block.data));
+			const auto maximum = _byteswap_ushort(*reinterpret_cast<uint16_t*>(block.data + 2));
 
-			if (!res)
+			if (maximum != CONSTANT_BLOCKSIZE || minimum != CONSTANT_BLOCKSIZE)
 			{
-				printf("Invalid streaminfo! Stream must have a constant blocksize of 1024 (was min: %i, max: %i)", minimum, maximum);
+				throw std::runtime_error(utils::string::va(
+					"Stream must have a constant blocksize of 1024! (was min: %i, max: %i)", 
+						minimum, maximum));
 			}
-
-			return res;
 		}
 
-		void convert_flac(const std::string& path)
+		void convert_flac(const std::string& path, const std::optional<std::string>& out_path)
 		{
 			std::string data{};
 			if (!utils::io::read_file(path, &data))
 			{
-				printf("Failed to read file %s\n", path.data());
-				return;
+				throw std::runtime_error("Failed to read file " + path);
 			}
 
 			if (!check_signature(data))
 			{
-				printf("File is not a flac file\n");
-				return;
+				throw std::runtime_error("File is not a flac file");
 			}
 
 			const auto start_pos = data.data();
@@ -139,9 +133,13 @@ namespace tool
 
 			metadata_block_t block{};
 
+			auto num_blocks = 0;
+			bool has_seektable = false;
+
 			while (!block.header.is_last)
 			{
 				parse_metadata_block(pos, &block);
+				num_blocks++;
 
 				if (!block.header.is_last)
 				{
@@ -153,50 +151,96 @@ namespace tool
 					const auto application_id = std::string{block.data, 4};
 					if (application_id == APPLICATION_ID)
 					{
-						printf("File has already been converted, aborting\n");
-						return;
+						throw std::runtime_error("File has already been converted, aborting\n");
 					}
 				}
 
 				if (block.header.type == block_type_t::streaminfo)
 				{
-					if (!verify_streaminfo_block(block))
-					{
-						return;
-					}
+					verify_streaminfo_block(block);
+				}
+
+				if (block.header.type == block_type_t::seektable)
+				{
+					has_seektable = true;
 				}
 			}
 
-			const auto insert_pos = static_cast<size_t>(pos - start_pos);
-			auto frame_section_size = (start_pos + data.size()) - (block.data + block.header.length);
-			auto header = 0x08000002; // application block, length 8
+			const auto frame_section_size = (start_pos + data.size()) - (block.data + block.header.length);
+			auto insert_pos = static_cast<size_t>(pos - start_pos);
+			auto insert_header = _byteswap_ulong(0x02000008); // application block, length 8
+
+			if (num_blocks == 1)
+			{
+				const auto header_ptr = reinterpret_cast<uint32_t*>(block.start);
+				const auto header = _byteswap_ulong(*header_ptr);
+				*header_ptr = _byteswap_ulong((header << 1) >> 1);
+
+				insert_header = _byteswap_ulong(0x82000008);
+				insert_pos = static_cast<size_t>(block.data - start_pos + block.header.length);
+			}
 
 			std::string insert_data{};
-			insert_data.append(reinterpret_cast<char*>(&header), 4); // header
+
+			if (!has_seektable)
+			{
+				printf("[Warning] Seektable not found! Adding empty seektable\n");
+				const auto seektable_ = _byteswap_ulong(0x03000000);
+				insert_data.append(reinterpret_cast<const char*>(&seektable_), 4);
+			}
+
+			insert_data.append(reinterpret_cast<const char*>(&insert_header), 4); // header
 			insert_data.append(APPLICATION_ID); // data (application id)
-			insert_data.append(reinterpret_cast<char*>(&frame_section_size), 4); // data (frame section size)
+			insert_data.append(reinterpret_cast<const char*>(&frame_section_size), 4); // data (frame section size)
 
 			std::string new_data = data;
 			new_data.insert(insert_pos, insert_data);
-
-			const auto last_dot = path.find_last_of('.');
-			const auto base_name = path.substr(0, last_dot);
-			const auto new_name = base_name + "_converted.flac";
+			
+			std::string new_name{};
+			if (out_path.has_value())
+			{
+				new_name = out_path.value();
+			}
+			else
+			{
+				const auto last_dot = path.find_last_of('.');
+				const auto base_name = path.substr(0, last_dot);
+				new_name = base_name + "_converted.flac";
+			}
 
 			utils::io::write_file(new_name, new_data, false);
 			printf("Conversion successful!\nSaved to %s\n", new_name.data());
+		}
+
+		void start_unsafe(int argc, char** argv)
+		{
+			if (argc < 2)
+			{
+				printf("Usage: flac-tool <flac file>\n");
+				return;
+			}
+
+			const auto path = argv[1];
+
+			auto output_path = utils::flags::get_flag("o");
+			if (!output_path.has_value())
+			{
+				output_path = utils::flags::get_flag("-output");
+			}
+
+			convert_flac(path, output_path);
 		}
 	}
 
 	void main(int argc, char** argv)
 	{
-		if (argc < 2)
+		try
 		{
-			printf("Usage: flac-tool <flac file>\n");
-			return;
+			start_unsafe(argc, argv);
 		}
-
-		const auto path = argv[1];
-		convert_flac(path);
+		catch (const std::exception& e)
+		{
+			printf("Conversion failed: %s\n", e.what());
+		}
 	}
 }
--- src/utils/flags.hpp
+++ src/utils/flags.hpp
@@ -5,4 +5,5 @@
 namespace utils::flags
 {
 	bool has_flag(const std::string& flag);
+	std::optional<std::string> get_flag(const std::string& flag);
 }
